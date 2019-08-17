9bbd2c6263bb32003993dca1bf8e3ca39a524e23
Rachel Keslensky <rach@lid.cc>
2019-08-16 19:38:04 -0700

    Setup CSS

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// wait for DOM to load before running JS
+$(function() {
+
+  //TODO: Display Current Date
+  //TODO: Convert to RickDate
+
+  //First, take the current year (which is often shortened to the last base-36 digit)
+  //Then the month
+  //Then the date
+});
